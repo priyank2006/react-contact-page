58dc58487e8e19b1528bcafc8b84eb0ab69c28af
Priyank <priyank.callistusblinds@gmail.com>
2023-08-26 14:36:58 +0530

    Navbar and Hero Section Done

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,16 @@
+import "../HeroSection.css";
+
+const HeroSection = () => {
+    return (
+        <div className="hero-section">
+            <div className="hero-main-content">
+                <h1> CONTACT US  </h1>
+
+                <p>Feel free to drop us a line with your suggestions, queries, complaints, or feedback. Our dedicated team is eager to assist you and provide the help you need. Your input is invaluable as we strive to improve and grow. Let's work together to make your experience with us even better.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default HeroSection
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <img src="https://marvellifestyle.com/wp-content/themes/MyCallistus/images/marvel%20logo.jpg" alt="" />
+            </div>
+
+            <ul>
+                <li href="#"> Home </li>
+                <li href="#"> Our Products </li>
+                <li href="#"> About Us </li>
+                <li href="#"> Contact Us </li>
+                <li href="#"> Our Reach </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar
